b440462ad1b161984aea774a79cf05a68918a047
REDJIL Abou Bakr Essadiq <52807987+Belloulzreg@users.noreply.github.com>
2022-03-18 00:34:05 -0700

    first commit

---
+++ angle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	double angle;
+	double s;
+	angle = 10*3.1459/180;
+	s = sin(angle);
+	cout<<"La valeur de sin(s) est : "<<s<<endl;
+	return 0;
+}
